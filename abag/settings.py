bb1ce20ece5467e39ff543a7d002119334739897
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-08-14 14:08:30 +0000

    storages added to settings

--- abag/settings.py
+++ abag/settings.py
@@ -180,3 +180,4 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
