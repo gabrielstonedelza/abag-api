1032d938af1a2d6b948678163547d850fdc71c96
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-08-14 10:26:37 +0000

    edited urls

--- abag/urls.py
+++ abag/urls.py
@@ -7,5 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
-    path('api/', include('users.urls')),
+    path('', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
