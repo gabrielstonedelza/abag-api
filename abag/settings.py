d9fb4e360424df65271de8d17f87a504e707ef64
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-08-14 13:51:45 +0000

    testing static and media files

--- abag/settings.py
+++ abag/settings.py
@@ -178,5 +178,12 @@ AWS_LOCATION = 'abag-static-folder'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
