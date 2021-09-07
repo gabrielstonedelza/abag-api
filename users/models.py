ee80248b434c6b9cd7dc33f933284c07f839e982
gabrielstonedelza <gabrielstonedelza@gmail.com>
2021-09-07 22:05:03 +0000

    rebalancing and agent branches added

--- users/models.py
+++ users/models.py
@@ -24,3 +24,24 @@ class User(AbstractUser):
     def get_agent_code(self):
         return self.agent_code
 
+
+class Branch(models.Model):
+    agent = models.ForeignKey(User, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True, max_length=255)
+    phone = models.CharField(max_length=15, unique=True)
+    user_code = models.CharField(max_length=11, unique=True)
+    full_name = models.CharField(max_length=150, default="Abag Agent User")
+
+    def __str__(self):
+        return self.agent.company_name
+
+
+class AgentsRebalancing(models.Model):
+    agent1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="requesting_agent")
+    agent2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="accepting_agent")
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    date_requested = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
